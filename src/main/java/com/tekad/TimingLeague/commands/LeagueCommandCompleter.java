2728ce694a65d1bb87fcf6a7ca487179381df3f4
Tekad <alessandroconnell@gmail.com>
2025-07-05 17:41:52 +0200

    tab completer updates

--- src/main/java/com/tekad/TimingLeague/commands/LeagueCommandCompleter.java
+++ src/main/java/com/tekad/TimingLeague/commands/LeagueCommandCompleter.java
@@ -8,9 +8,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-// i made chatgpt do this as i am tired
 public class LeagueCommandCompleter implements TabCompleter {
 
     private final Map<String, League> leagues = TImingLeague.getLeagueMap();
@@ -18,36 +19,70 @@ public class LeagueCommandCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> completions = new ArrayList<>();
+        boolean isAdmin = sender.hasPermission("timingleague.admin");
 
         if (args.length == 1) {
-            completions.add("create");
+            if (isAdmin) {
+                completions.add("create");
+                completions.add("delete");
+            }
             completions.add("help");
             completions.addAll(leagues.keySet()); // league names
         }
 
         else if (args.length == 2 && leagues.containsKey(args[0])) {
-            completions.addAll(List.of(
-                    "addDriver", "addEvent", "calendar", "updateWithHeat",
-                    "standings", "holo", "team", "scoring"
-            ));
+            // All users can access these
+            completions.add("team");
+            completions.add("standings");
+            completions.add("help");
+            completions.add("team standings");
+
+            if (isAdmin) {
+                // Admin only commands
+                completions.add("addDriver");
+                completions.add("removeDriver");
+                completions.add("update");
+                completions.add("calendar");
+                completions.add("scoring");
+                completions.add("holo"); // hologram commands
+                completions.add("team delete");
+                completions.add("team create");
+                completions.add("team setColor");
+                completions.add("team setName");
+            } else {
+                // For non-admins, include only allowed team subcommands here
+                completions.add("team create");
+                completions.add("team delete");
+                completions.add("team setColor");
+                completions.add("team setName");
+            }
         }
 
         else if (args.length == 3 && leagues.containsKey(args[0])) {
             String sub = args[1].toLowerCase();
-            if (sub.equals("adddriver")) {
-                for (OfflinePlayer p : Bukkit.getOfflinePlayers()) {
-                    if (p.getName() != null) completions.add(p.getName());
+
+            if (isAdmin) {
+                if (sub.equals("adddriver")) {
+                    for (OfflinePlayer p : Bukkit.getOfflinePlayers()) {
+                        if (p.getName() != null) completions.add(p.getName());
+                    }
+                } else if (sub.equals("removeDriver")) {
+                    for (OfflinePlayer p : Bukkit.getOfflinePlayers()) {
+                        if (p.getName() != null) completions.add(p.getName());
+                    }
+                } else if (sub.equals("update") || sub.equals("calendar")) {
+                    completions.add("<eventId>");
+                } else if (sub.equals("holo")) {
+                    completions.addAll(List.of("driver", "team", "update", "deleteClosest"));
+                } else if (sub.equals("scoring")) {
+                    completions.addAll(List.of("fc1", "fc2", "default"));
                 }
-            } else if (sub.equals("addevent") || sub.equals("updatewithheat")) {
-                completions.add("<eventId>");
-            } else if (sub.equals("holo")) {
-                completions.addAll(List.of("driver", "team", "update","deleteClosest"));
-            } else if (sub.equals("team")) {
+            }
+
+            if (sub.equals("team")) {
                 completions.addAll(List.of("create", "color", "add", "remove", "view"));
-                completions.addAll(leagues.get(args[0]).getTeamsString()); // team names
-            } else if (sub.equals("scoring")){
-                completions.addAll(List.of("fc1","fc2","default"));
-            } else if (sub.equals("standings")){
+                completions.addAll(leagues.get(args[0]).getTeamsString());
+            } else if (sub.equals("standings")) {
                 completions.addAll(List.of("teams"));
             }
         }
@@ -56,23 +91,27 @@ public class LeagueCommandCompleter implements TabCompleter {
             String teamSub = args[2].toLowerCase();
             if (teamSub.equals("create")) {
                 completions.add("<teamName>");
-            } else if (teamSub.equals("color")) {
+            } else if (teamSub.equals("color") || teamSub.equals("setColor")) {
                 completions.addAll(leagues.get(args[0]).getTeamsString());
             } else if (teamSub.equals("add") || teamSub.equals("remove")) {
                 completions.addAll(leagues.get(args[0]).getTeamsString());
             } else if (teamSub.equals("view")) {
+                completions.addAll(leagues.get(args[0]).getTeamsString());
+            } else if (teamSub.equals("setName")) {
                 completions.addAll(leagues.get(args[0]).getTeamsString());
             }
         }
 
         else if (args.length == 5 && leagues.containsKey(args[0]) && args[1].equalsIgnoreCase("team")) {
             String teamSub = args[2].toLowerCase();
-            if (teamSub.equals("color")) {
+            if (teamSub.equals("color") || teamSub.equals("setColor")) {
                 completions.add("#RRGGBB");
             } else if (teamSub.equals("add") || teamSub.equals("remove")) {
                 for (OfflinePlayer p : Bukkit.getOfflinePlayers()) {
                     if (p.getName() != null) completions.add(p.getName());
                 }
+            } else if (teamSub.equals("setName")) {
+                completions.add("<newTeamName>");
             }
         }
 
